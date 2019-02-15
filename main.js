42dd77699bbe68bdd5d08bad417e305e9f932e2b
Lex Aeterna <lexaeterna12321@gmail.com>
2019-02-15 13:12:21 +0100

    photo swap / minor adjustments

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const main = document.querySelector("main");
 
 const onloadImgConfig = { filter: "blur(0px)", opacity: 1 };
-
+let imgNumber = 1;
 window.addEventListener("DOMContentLoaded", () => {
   for (let prop in onloadImgConfig) {
     main.style[prop] = onloadImgConfig[prop];
